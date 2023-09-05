3797f6a2463a08da4f9e50e80e759d40311586c3
Mwadali <wellingtonmwadali@gmail.com>
2023-09-06 00:45:38 +0300

    deleted src

--- extension/public/popup.js
+++ extension/public/popup.js
@@ -1,37 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ColorGrid from './components/ColorGrid';
-
 document.addEventListener('DOMContentLoaded', () => {
-  const colorGridElement = document.getElementById('root');
+  const colorSelectors = document.querySelectorAll('.color-option');
+  const chrome = window.chrome;
 
-  colorGridElement.addEventListener('click', (event) => {
-    const color = event.target.style.backgroundColor;
-    const chrome = window.chrome;
-    if (color) {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.scripting.executeScript({
-          target: { tabId: tabs[0].id },
-          function: changeBackgroundColor,
-          args: [color],
+  // Add click event listeners to all color selectors
+  colorSelectors.forEach((colorSelector) => {
+    colorSelector.addEventListener('click', (event) => {
+      const color = event.target.style.backgroundColor;
+
+      if (color) {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+          chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            function: changeBackgroundColor,
+            args: [color],
+          });
         });
-      });
-    }
+      }
+    });
   });
 
   function changeBackgroundColor(color) {
     document.body.style.backgroundColor = color;
   }
-
-  // Initialize the color grid React component
-  const colorGridContainer = document.createElement('div');
-  colorGridContainer.setAttribute('id', 'color-grid-container');
-  // Append the color grid div to the 'root' div
-    const rootElement = document.getElementById('root');
-    if (rootElement) {
-      rootElement.appendChild(colorGridContainer);
-    }
-  // Render the ColorGrid component into the container
-  ReactDOM.render(<ColorGrid />, colorGridContainer);
 });
-
