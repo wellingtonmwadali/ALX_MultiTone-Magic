5b0e4decbe76c4a4f0709acdb62b7d1ad62f81e4
Mwadali <wellingtonmwadali@gmail.com>
2023-09-06 22:44:47 +0300

    created background.js file

--- extension/public/popup.js
+++ extension/public/popup.js
@@ -1,25 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const colorSelectors = document.querySelectorAll('.color-option');
-  const chrome = window.chrome;
+console.log("Popup script is running!");
+document.addEventListener('DOMContentLoaded', function () {
+  const colorOptions = document.querySelectorAll('.color-option');
 
-  // Add click event listeners to all color selectors
-  colorSelectors.forEach((colorSelector) => {
-    colorSelector.addEventListener('click', (event) => {
-      const color = event.target.style.backgroundColor;
+  // Add a click event listener to each color circle
+  colorOptions.forEach((colorOption) => {
+    colorOption.addEventListener('click', function () {
+      // Get the background color of the clicked color circle
+      const backgroundColor = window.getComputedStyle(colorOption).backgroundColor;
 
-      if (color) {
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          chrome.scripting.executeScript({
-            target: { tabId: tabs[0].id },
-            function: changeBackgroundColor,
-            args: [color],
-          });
-        });
-      }
+      // Send a message to the background script to change the background color
+      chrome.runtime.sendMessage({ action: 'changeBackgroundColor', color: backgroundColor });
     });
   });
-
-  function changeBackgroundColor(color) {
-    document.body.style.backgroundColor = color;
-  }
 });
+
+
