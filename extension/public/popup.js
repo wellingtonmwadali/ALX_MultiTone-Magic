87cac565445b9315bcb9abf9fb3374331504b131
Mwadali <wellingtonmwadali@gmail.com>
2023-10-18 21:04:45 +0300

    added data-themes

--- extension/public/popup.js
+++ extension/public/popup.js
@@ -4,29 +4,36 @@ document.addEventListener('DOMContentLoaded', function () {
   const switchElement = document.getElementById('theme-switch');
   const codeSnippetElement = document.getElementById('codeSnippet');
 
-  // Retrieve the stored theme switch state and selected theme from localStorage
-  const themeSwitchState = localStorage.getItem('themeSwitchState');
-  const selectedTheme = localStorage.getItem('selectedTheme');
-
-  // Apply the stored theme switch state and selected theme on page load
-  if (themeSwitchState === 'on') {
-    codeSnippetElement.style.display = 'block';
-    switchElement.checked = true;
-    if (selectedTheme) {
-      setTheme(selectedTheme); // Apply the saved theme
-    }
-  } else {
-    codeSnippetElement.style.display = 'none';
-    switchElement.checked = false;
+  // Function to set the theme and save it to localStorage
+  function setThemeAndStore(themeName) {
+    document.documentElement.className = themeName;
+    localStorage.setItem('selectedTheme', themeName);
   }
+
+  // Function to retrieve the stored theme from localStorage
+  function getStoredTheme() {
+    return localStorage.getItem('selectedTheme');
+  }
+
+  // Function to apply the stored theme on page load
+  function applyStoredTheme() {
+    const storedTheme = getStoredTheme();
+    if (storedTheme) {
+      setThemeAndStore(storedTheme);
+    }
+  }
+
+  // Apply the stored theme on page load
+  applyStoredTheme();
 
   // Add an event listener to the theme switch
   switchElement.addEventListener('change', function () {
     if (this.checked) {
       codeSnippetElement.style.display = 'block';
       localStorage.setItem('themeSwitchState', 'on');
+      const selectedTheme = getStoredTheme();
       if (selectedTheme) {
-        setTheme(selectedTheme); // Apply the saved theme when the switch is turned on
+        setThemeAndStore(selectedTheme);
       }
     } else {
       codeSnippetElement.style.display = 'none';
@@ -43,23 +50,13 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
-  // When the user selects a theme
-  function setTheme(themeName) {
-    document.documentElement.className = themeName;
-    localStorage.setItem('selectedTheme', themeName); // Save the selected theme
-  }
-
-  // When the page loads, check the extension's storage for the saved theme
-  if (selectedTheme && themeSwitchState === 'on') {
-    setTheme(selectedTheme);
-  }
-
+  // Function to show the name of the selected color (not provided in the code).
   function showColorName(color) {
     document.getElementById('color-name').innerText = `Color: ${color}`;
   }
 
+  // Function to hide the name of the selected color (not provided in the code).
   function hideColorName() {
     document.getElementById('color-name').innerText = '';
   }
 });
- mwadali
