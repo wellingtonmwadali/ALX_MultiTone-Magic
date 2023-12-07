2a5eed5367ee7ea767492e89892d1095a6e11224
Mwadali <wellingtonmwadali@gmail.com>
2023-12-07 16:02:32 +0300

    fixed background to send message

--- extension/public/background.js
+++ extension/public/background.js
@@ -1,4 +1,7 @@
-//Set receiving end for popup.js when user changes theme
+/// Background Script
+
+let isExtensionEnabled = true; // Default state
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === 'setTheme') {
         // Update the stored theme
@@ -9,8 +12,36 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
                 console.log('Stored theme updated:', request.theme);
             }
         });
+    } else if (request.action === 'toggleExtension') {
+        // Update the extension state
+        isExtensionEnabled = request.isEnabled;
+
+        // If the extension is now disabled, remove the stored theme
+        if (!isExtensionEnabled) {
+            chrome.storage.local.remove('currentTheme', function() {
+                if (chrome.runtime.lastError) {
+                    console.error(chrome.runtime.lastError);
+                } else {
+                    console.log('Stored theme removed.');
+                }
+            });
+        }
+
+        // Broadcast the state change to all tabs
+        chrome.tabs.query({}, function(tabs) {
+            tabs.forEach(function(tab) {
+                // Send a message to the content script in each tab
+                chrome.tabs.sendMessage(tab.id, { action: 'updateExtensionState', isEnabled: isExtensionEnabled }, function(response) {
+                    // Handle response if needed
+                    if (chrome.runtime.lastError) {
+                        console.error('Error sending message:', chrome.runtime.lastError.message);
+                    }
+                });
+            });
+        });
     }
 });
+
 // Set the default theme based on user's selection or use a default value
 chrome.storage.local.get(['currentTheme'], function(result) {
     const defaultTheme = result.currentTheme || 'default_theme';
--- extension/public/content.js
+++ extension/public/content.js
@@ -3,9 +3,8 @@
 function applyTheme(theme) {
     if (theme) {
         document.body.classList.add(`theme-${theme}`);
-    }
-    else {
-        console.log("error in loading theme");
+    } else {
+        console.log("Error in loading theme");
     }
 }
 
@@ -20,6 +19,25 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
 chrome.storage.local.get(['currentTheme'], function(result) {
     applyTheme(result.currentTheme);
 });
+
+// Function to update extension state
+function updateExtensionState(isEnabled) {
+    if (!isEnabled) {
+        // Clear any applied theme when the extension is disabled
+        document.body.classList.remove('defaultTheme', 'theme-red', 'theme-green', 'theme-navy-blue','theme-black','theme-teal',
+        'theme-ocean','theme-purple','theme-pink','theme-orange', 'theme-yellow','theme-grey');
+    }
+}
+
+// Listen for changes in the extension state
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === 'updateExtensionState') {
+        updateExtensionState(request.isEnabled);
+    }
+});
+
+// Assume the initial extension state is enabled
+updateExtensionState(true);
 
 
 
